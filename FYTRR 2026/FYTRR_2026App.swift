9b1b0f912e80594e96e928dbb0e15613c1c30513
Edwin Mendoza <Edoza017@gmail.com>
2026-04-07 23:34:46 -0700

    Initial Commit

---
+++ FYTRR 2026/FYTRR_2026App.swift
@@ -0,0 +1,17 @@
+//
+//  FYTRR_2026App.swift
+//  FYTRR 2026
+//
+//  Created by EDWIN MENDOZA on 4/7/26.
+//
+
+import SwiftUI
+
+@main
+struct FYTRR_2026App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FYTRR 2026Tests/FYTRR_2026Tests.swift
@@ -0,0 +1,17 @@
+//
+//  FYTRR_2026Tests.swift
+//  FYTRR 2026Tests
+//
+//  Created by EDWIN MENDOZA on 4/7/26.
+//
+
+import Testing
+@testable import FYTRR_2026
+
+struct FYTRR_2026Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
